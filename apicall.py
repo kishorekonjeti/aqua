2ff5aef9e0c6775a312a0f8bf992a6d2d7b90f9a
Kishore Konjeti <kishore.konjeti@gmail.com>
2021-10-26 11:04:12 +0530

    Add files via upload
    updated code

--- apicall.py
+++ apicall.py
@@ -6,17 +6,43 @@ def doGet(url, headers):
     """
     This is a low level get method includes paramers , any other business logic which will require to run for all get calls
     """
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
 
-    response = requests.get(url, headers=headers)
+        return response.status_code
 
-    return response.status_code
+
+
+    except requests.exceptions.HTTPError as errh:
+        print(errh)
+    except requests.exceptions.ConnectionError as errc:
+        print(errc)
+    except requests.exceptions.Timeout as errt:
+        print(errt)
+    except requests.exceptions.RequestException as err:
+        print(err)
+
+   
 
 
 def doPost(url, headers):
     """
     This is a low level Post method includes paramers , any other business logic which will require to run for all Post calls
     """
+    try:
+        response = requests.post(url, headers=headers, data=None)
+        response.raise_for_status()
 
-    response = requests.post(url, headers=headers, data=None)
+        return response.status_code
+    
+    except requests.exceptions.HTTPError as errh:
+        print(errh)
+    except requests.exceptions.ConnectionError as errc:
+        print(errc)
+    except requests.exceptions.Timeout as errt:
+        print(errt)
+    except requests.exceptions.RequestException as err:
+        print(err)
 
     return response.status_code
